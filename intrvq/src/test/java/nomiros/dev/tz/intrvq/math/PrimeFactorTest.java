36dd28c82a09f123084d072c53a6a891a5e8c3a1
Amir Noy <anoy@cisco.com>
2018-03-29 10:47:11 +0300

    adding PrimeFactorTest + rename intvq to intrvq

---
+++ intrvq/src/test/java/nomiros/dev/tz/intrvq/math/PrimeFactorTest.java
@@ -0,0 +1,46 @@
+package nomiros.dev.tz.intrvq.math;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * PrimeFactorTest
+ * 
+ * calculate the prime factors of a given positive integer > 1
+ * 
+ * 
+ * @author anoy
+ *
+ */
+public class PrimeFactorTest {
+
+    @Test
+    public void testPrimeFactor() {
+        
+        Assert.assertEquals("test 1:  ", null, primeFactorsOf(0));
+        
+        
+    }
+
+    
+    
+    /**
+     * 
+     * @param i
+     * @return
+     */
+    private int[] primeFactorsOf(int i) {
+       
+        
+        
+        
+        return null;
+    }
+
+    
+    
+    
+    
+    
+    
+}
